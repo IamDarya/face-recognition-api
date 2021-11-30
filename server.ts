95137847261afce61b4b5c771517764d230dafbd
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-11-30 22:36:54 +0300

    feat: add PostgreSQL database to server functions

--- server.ts
+++ server.ts
@@ -1,85 +1,103 @@
+const cors = require("cors");
+const hash = require("bcrypt-nodejs");
+const bcrypt = require("bcrypt-nodejs");
 const express = require("express");
 const bodyParser = require("body-parser");
+const knex = require("knex");
+
+const DBpostgres = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    port: 5432,
+    database: "facerec",
+  },
+});
 
 const app = express();
 app.use(bodyParser.json());
-
-const database = {
-  users: [
-    {
-      id: "122",
-      name: "John",
-      email: "johnEmail@gmail.com",
-      password: "myPassowrd123",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Sally",
-      email: "sallyEmail@gmail.com",
-      password: "SallyPassowrd123",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
+app.use(cors());
 
 app.get("/", (req: any, res: any) => {
-  res.send(database.users);
+  res.send("success");
 });
 
-app.post("/signin", (req: any, res: any) => {
-  let userData = database.users.filter((el) => el.email === req.body.email);
-  if (userData.length && userData[0].password === req.body.password) {
-    res.json("sing in successfully!");
-  } else {
-    res.status(400).json("error to login");
-  }
+app.post("/signIn", (req: any, res: any) => {
+  DBpostgres.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data: any) => {
+      const validLogin = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (validLogin) {
+        return DBpostgres.select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user: any) => {
+            res.json(user[0]);
+          })
+          .catch((err: Error) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong login data");
+      }
+    })
+    .catch((err: Error) => res.status(400).json("wrong login data"));
 });
 
 app.post("/register", (req: any, res: any) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: "123",
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(database.users[database.users.length - 1]);
+  const hash = bcrypt.hashSync(password);
+  DBpostgres.transaction((trx: any) => {
+    trx
+      .insert({ hash: hash, email: email })
+      .into("login")
+      .returning("email")
+      .then((loginEmail: any) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date(),
+          })
+          .then((user: any) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err: Error) => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req: any, res: any) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(404).json("not found");
-  }
+  DBpostgres.select("*")
+    .from("users")
+    .where({ id })
+    .then((user: any) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("user not found");
+      }
+    });
 });
 
 app.put("/image", (req: any, res: any) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    // can be moved in a function REPEAT
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(404).json("not found");
-  }
+  DBpostgres("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries: any) => {
+      if (entries.length) {
+        res.json(entries);
+      } else {
+        res.status(400).json("entries not found");
+      }
+    });
 });
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
